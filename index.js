d8049ef73542bb1ada3e5ee70f63d07f8f1a3393
Allan <github email address>
2022-06-09 19:48:22 +0300

    included code for 400 - 2000

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function scuberGreetingForFeet(ride) {
 
     return ("This one is on me!");
 
+  } else if (ride > 400 && ride <= 2000) {
+
+    return ("That will be 20 bucks.");
+
   } else if (ride > 2000 && ride <2500) {
 
     return ("I will gladly take your thirty bucks.");
